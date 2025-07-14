5ad968e22de18b2dc6a9b322386d68ba35430f17
Sarah Jackson <sarah.j@buildkite.com>
2025-07-14 14:23:36 +1000

    Remove tracked build files now ignored by .gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /app
-# Build directory: /app/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(RunTest "/app/build/test_hello")
-set_tests_properties(RunTest PROPERTIES  _BACKTRACE_TRIPLES "/app/CMakeLists.txt;16;add_test;/app/CMakeLists.txt;0;")
